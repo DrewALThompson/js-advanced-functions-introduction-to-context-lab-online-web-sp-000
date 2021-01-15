7ea8813096e9e88263b13089bd36cb2d029438e2
Drew Thompson <drewallynthompson@gmail.com>
2021-01-15 08:19:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ function createEmployeeRecord(arr){
   let record = {
     firstName: nil,
     familyName: nil,
-    
+    title: nil,
+    payPerHour: nil,
+    timeInEvents: [], 
+    timeOutEvents []:
   }
 }
