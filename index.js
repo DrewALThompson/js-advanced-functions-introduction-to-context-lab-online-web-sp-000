b44c6e117d005341cbaf6b3039354a31c039f686
Drew Thompson <drewallynthompson@gmail.com>
2021-01-15 08:50:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,6 +29,6 @@ function createTimeOutEvent(record, dateStamp){
 }
 
 function hoursWorkedOnDate(record, workDate){
-  let timeIn = record.timeInEvents.find(day => time === workDate)
+  let timeIn = record.timeInEvents.find(event => event.day === workDate)
 }
 
